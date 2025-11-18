b79eb80e8b80c0e0ce8994015342a85df96d60e4
SaiKalyan-07 <skravikumar2004@gmail.com>
2025-11-18 14:28:28 +0530

    feat: add real-time chat system using Socket.io with chat models, routes, and socket server integration

--- src/app.js
+++ src/app.js
@@ -2,7 +2,18 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app= express();
 const cookieParser = require("cookie-parser");
-require('dotenv').config()
+const cors = require("cors");
+const http = require("http");
+
+require('dotenv').config();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -10,18 +21,24 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
 
 app.use("/",authRouter);
 app.use("/",profileRouter);
 app.use("/",requestRouter);
 app.use("/",userRouter);
+app.use("/",chatRouter);
+
+const server = http.createServer(app);
+initializeSocket(server);
 
 
 
 connectDB()
   .then(() => {
     console.log("Database connection established...");
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log("Server is successfully listening on port 7777...");
     });
   })
